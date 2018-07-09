746c9797e596bb8e9435dc29919cda030e1ad25a
Alex Busch <abusch1013@gmail.com>
2018-07-09 09:11:36 -0400

    Updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,63 @@
-let gulp = require('gulp');
-let imagemin = require('gulp-imagemin');
-let pngquant = require('imagemin-pngquant');
+var gulp = require('gulp');
+var babelify = require('babelify');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var cssbeautify = require('gulp-cssbeautify');
+var browserSync = require('browser-sync').create();
+var image = require('gulp-image');
 
-gulp.task('default', ['imagemin']);
-
-gulp.task('imagemin', function() {
- return gulp.src('img/bigboys/*')
- .pipe(imagemin({
-  progressive: true,
-  use: [pngquant()],
-  }))
-  .pipe(gulp.dest('img'));
+gulp.task('css', function() {
+    return gulp.src('./css/*.css')
+    .pipe(cssbeautify())
+    .pipe(gulp.dest('./css/'))
+    .pipe(browserSync.stream());
 });
+
+gulp.task('scripts:main', function() {
+  browserify(['js/main.js', 'js/dbhelper.js', 'js/app.js', './sw.js'])
+    .bundle()
+    .pipe(source('main_bundle.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(sourcemaps.write('maps'))
+    .pipe(gulp.dest('./bundle_js'));
+});
+
+gulp.task('scripts:restaurant', function() {
+  browserify(['js/restaurant_info.js', 'js/dbhelper.js', './app.js', './sw.js'])
+    .bundle()
+    .pipe(source('restaurant_bundle.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(sourcemaps.write('maps'))
+    .pipe(gulp.dest('dist/js'))
+    .pipe(gulp.dest('./bundle_js'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(['./sw.js', './js/**/*.js'], ['scripts:main', 'scripts:restaurant']);
+});
+
+gulp.task('serve', ['css'], function() {
+  browserSync.init({
+    server: './',
+    browser: 'google chrome'
+  });
+
+  gulp.watch('./**/**.html').on('change', browserSync.reload);
+  gulp.watch('./bundle_js/**/*.js').on('change', browserSync.reload);
+
+});
+
+gulp.task('copy-files', function() {
+  gulp.src(['./index.html', './restaurant.html', 'manifest.json'])
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('dist', ['copy-files', 'css', 'scripts:main', 'scripts:restaurant']);
+gulp.task('default', ['scripts:main', 'scripts:restaurant', 'watch', 'serve']);
--- sw.js
+++ sw.js
@@ -13,17 +13,7 @@ self.addEventListener('install', event => {
       '/js/main.js',
       '/js/restaurant_info.js',
       '/css/styles.css',
-      '/img/1.webp',
-      '/img/2.webp',
-      '/img/3.webp',
-      '/img/4.webp',
-      '/img/5.webp',
-      '/img/6.webp',
-      '/img/7.webp',
-      '/img/8.webp',
-      '/img/9.webp',
-      '/img/10.webp',
-      '/maps/*';
+      '/img'
      ]);
    })
  );
